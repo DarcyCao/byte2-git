4fabfc540c9c3b74f34fe7b034dae73a32cef144
hetianc <hetianc@andrew.cmu.edu>
2017-02-04 12:28:16 -0500

    headache

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ import urllib
 
 # this is used for constructing URLs to google's APIS
 from googleapiclient.discovery import build
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+API_KEY = AIzaSyArQBTXqntHoemT5ufnTbMc2q1hMDzicoM
+service = build('fusiontables', 'v1', developerKey=API_KEY)
+
+
+#depression
+TABLE_ID = '1U4GFT7969hZ9q73-lAdbCjCGgkb__ElT4TcF7o8'
+
+request = service.column().list(tableId=TABLE_ID) 
 
 
 @app.route('/')
